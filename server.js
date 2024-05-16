1aa3ebc9e103012cc8f2521294eacc2ae5415732
lav3ll <lavell.francis@hotmail.com>
2024-05-16 22:42:06 +0100

    server files setup with nodemon

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+
+config = require('./config');
+
+app.listen(
+  config.port,
+  console.log('Server has started on port http://localhost:%s', config.port)
+);
