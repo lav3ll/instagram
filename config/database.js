19ae7909f4d2512215dbd6db2d53b8c37e3468bb
lav3ll <lavell.francis@hotmail.com>
2024-05-22 16:41:28 +0100

    replaced mongodb with mysql

--- config/database.js
+++ config/database.js
@@ -1,9 +1,21 @@
-const mongoose = require('mongoose');
+const mysql = require('mysql2/promise');
 const config = require('./index');
 
-const db = mongoose
-  .connect(config.mongo_uri, { useNewUrlParser: true })
-  .then(() => console.log('connected to database'))
-  .catch((err) => console.error('An error has occured', err));
+async function connectToDatabase() {
+  try {
+    const connection = await mysql.createConnection({
+      host: config.db_host,
+      user: config.db_user,
+      password: config.db_password,
+      database: config.db_name,
+    });
+    console.log('Connected to the MySQL database');
+    return connection;
+  } catch (err) {
+    console.error('An error has occurred', err);
+  }
+}
+
+const db = connectToDatabase();
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+
+const mysql = require('mysql');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 
 config = require('./config');
+
+const db = require('./config/database');
 
 app.listen(
   config.port,
