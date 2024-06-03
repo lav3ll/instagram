65d58f454113d35c251776eb08d0021ff4abcf13
lav3ll <lavell.francis@hotmail.com>
2024-06-04 00:17:42 +0100

    removed vite boiler plate values

--- config/database.test.js
+++
@@ -1,7 +0,0 @@
-const database = require('./database');
-
-test('it contains mysql and config imports', () => {
-  const mysqlTest = '';
-  const myconfigTest = config;
-  expect(mysqlTest, myconfigTest).toBeDentified();
-});
--- config/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 3000,
-  db_host: process.env.DB_HOST || 'localhost',
-  db_user: process.env.DB_USER || 'root',
-  db_password: process.env.DB_PASSWORD || '',
-  db_name: process.env.DB_NAME || 'instagram-db',
-  secret: process.env.SECRET || 'CodingIsCool',
-};
