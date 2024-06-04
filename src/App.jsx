8633db156183540f7a77b0926fd19707a44275ee
lav3ll <lavell.francis@hotmail.com>
2024-06-04 23:13:00 +0100

    installed and tested light/dark mode background color changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Button } from '@chakra-ui/react';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button>Hello world</Button>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
+import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode('gray.100', '#000')(props),
+      color: mode('gray.800', 'white', 'whiteAlpha.900')(props),
+    },
+  }),
+};
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config, styles });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
