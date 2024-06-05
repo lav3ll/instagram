614bc653b7e62804f15fe2cda40857a923581857
lav3ll <lavell.francis@hotmail.com>
2024-06-05 22:34:03 +0100

    imported needed components to authform file and created input elements

--- src/Components/AuthForm/AuthForm.jsx
+++ src/Components/AuthForm/AuthForm.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Box, VStack, Image, Input } from '@chakra-ui/react';
 
 const AuthForm = () => {
-  return <div>AuthForm</div>;
+  return (
+    <Box border={'1px solid'} borderRadius={'4'} padding={5}>
+      <VStack spacing={4}>
+        <Image src='/logo.png' h={24} cursor='pointer' alt='instagram' />
+        <Input placeholder='Email' onChange={''} fontSize={14} type='email' />
+        <Input
+          placeholder='Password'
+          onChange={''}
+          fontSize={14}
+          type='password'
+        />
+      </VStack>
+    </Box>
+  );
 };
 
 export default AuthForm;
