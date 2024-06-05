f9419f09471b0b4156dfbebb26dc77e22dbdcc6e
lav3ll <lavell.francis@hotmail.com>
2024-06-05 23:12:50 +0100

    installed react dev testin library

--- src/Components/AuthForm/AuthForm.jsx
+++ src/Components/AuthForm/AuthForm.jsx
@@ -12,50 +12,70 @@ import {
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   return (
-    <Box border={'1px solid'} borderRadius={'4'} padding={5}>
-      <VStack spacing={4}>
-        <Image src='/logo.png' h={24} cursor='pointer' alt='instagram' />
-        <Input placeholder='Email' onChange={''} fontSize={14} type='email' />
-        <Input
-          placeholder='Password'
-          onChange={''}
-          fontSize={14}
-          type='password'
-        />
-        {!isLogin ? (
-          <Input placeholder='Confirm Password' fontSize={14} type='password' />
-        ) : null}
-        <Button w={'full'} colorScheme={'blue'} size={'sm'} fontSize={14}>
-          {isLogin ? 'login' : 'Sign Up'}
+    <>
+      <Box border={'1px solid'} borderRadius={'4'} padding={5}>
+        <VStack spacing={4}>
+          <Image src='/logo.png' h={24} cursor='pointer' alt='instagram' />
+          <Input placeholder='Email' onChange={''} fontSize={14} type='email' />
+          <Input
+            placeholder='Password'
+            onChange={''}
+            fontSize={14}
+            type='password'
+          />
+          {!isLogin ? (
+            <Input
+              placeholder='Confirm Password'
+              fontSize={14}
+              type='password'
+            />
+          ) : null}
+          <Button w={'full'} colorScheme={'blue'} size={'sm'} fontSize={14}>
+            {isLogin ? 'login' : 'Sign Up'}
 
-          {/* Or text */}
-        </Button>
-        <Flex
-          alignItems={'center'}
-          justifyContent={'center'}
-          my={4}
-          gap={1}
-          w={'full'}
-        >
-          <Box flex={2} h={'1px'} bg={'gray.400'} />
-          <Text mx={1} color={'white'}>
-            OR
-          </Text>
-          <Box flex={2} h={'1px'} bg={'gray.400'} />
-        </Flex>
+            {/* Or text */}
+          </Button>
+          <Flex
+            alignItems={'center'}
+            justifyContent={'center'}
+            my={4}
+            gap={1}
+            w={'full'}
+          >
+            <Box flex={2} h={'1px'} bg={'gray.400'} />
+            <Text mx={1} color={'white'}>
+              OR
+            </Text>
+            <Box flex={2} h={'1px'} bg={'gray.400'} />
+          </Flex>
 
-        <Flex
-          alignItems={'center'}
-          justifyContent={'center'}
-          cursor={'pointer'}
-        >
-          <Image src='/google.png' w={5} alt='google logo' />
-          <Text mx={2} color={'white'}>
-            Log in with google
-          </Text>
+          <Flex
+            alignItems={'center'}
+            justifyContent={'center'}
+            cursor={'pointer'}
+          >
+            <Image src='/google.png' w={5} alt='google logo' />
+            <Text mx={2} color={'white'}>
+              Log in with google
+            </Text>
+          </Flex>
+        </VStack>
+      </Box>
+      <Box border={'1px solid gray'} borderRadius={4}>
+        <Flex alignItems={'center'} justifyContent={'center'}>
+          <Box mx={2} fontSize={14}>
+            {isLogin ? "Don't have an account" : 'Already have an account?'}
+          </Box>
+          <Box
+            onClick={() => setIsLogin(!isLogin)}
+            color={'blue.500'}
+            cursor={'pointer'}
+          >
+            {isLogin ? 'Sign Up' : 'Log in'}
+          </Box>
         </Flex>
-      </VStack>
-    </Box>
+      </Box>
+    </>
   );
 };
 
