6d937b40a467fd36262b5428ecd7fd3e9e59f2f2
lav3ll <lavell.francis@hotmail.com>
2024-06-11 13:42:49 +0100

    created sidebar base file

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Flex } from '@chakra-ui/react';
+import Sidebar from '../../Components/Sidebar/Sidebar';
 
-const PageLayout = () => {
-  return <div>PageLayout</div>;
+const PageLayout = ({ children }) => {
+  const pathname = useLocation();
+  return (
+    <Flex>
+      {/* sidebar on the left */}
+      <Box w={{ base: '70px', md: '240px' }}>
+        <Sidebar />
+      </Box>
+      {/* right side content */}
+      <Box>{children}</Box>
+    </Flex>
+  );
 };
 
 export default PageLayout;
