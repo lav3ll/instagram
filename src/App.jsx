f43ff28eb3e41b1ac6eb0c5b4ca8971019e032f7
lav3ll <lavell.francis@hotmail.com>
2024-06-11 14:12:29 +0100

    updated app.js to be wrapped in pagelayout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './Components/HomePage';
 import AuthPage from './Components/AuthPage';
+import PageLayout from './Layouts/PageLayout/PageLayout';
 
 function App() {
   return (
-    <>
+    <PageLayout>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='auth' element={<AuthPage />} />
       </Routes>
-    </>
+    </PageLayout>
   );
 }
 
--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Box } from '@chakra-ui/react';
 import Sidebar from '../../Components/Sidebar/Sidebar';
+import { useLocation } from 'react-router-dom';
 
 const PageLayout = ({ children }) => {
   const pathname = useLocation();
   return (
     <Flex>
       {/* sidebar on the left */}
-      // if there path is not equal to auth, render sidebar
       {pathname !== '/auth' ? (
         <Box w={{ base: '70px', md: '240px' }}>
           <Sidebar />
         </Box>
       ) : null}
       {/* right side content */}
-      <Box>{children}</Box>
+      <Box flex={1} w={{ base: 'calc(100%-70px)', md: 'calc(100%-240px)' }}>
+        {children}
+      </Box>
     </Flex>
   );
 };
