2eb986bb3787ed537f1d497f06b1c8b44bc0dd94
lav3ll <lavell.francis@hotmail.com>
2024-06-12 22:37:49 +0100

    imported more icons to be used for the sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,40 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Avatar, Box, Flex, Link } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 import React from 'react';
+import AiFillHome from 'react-icons/ai';
+import {
+  CreatePostLogo,
+  InstagramLogo,
+  InstagramMobileLogo,
+  NotificationsLogo,
+  SearchLogo,
+} from '../../assets/constants';
 
 const Sidebar = () => {
+  const sidebarItems = [
+    {
+      icon: <AiFillHome size={25} />,
+      text: 'Home',
+      link: '/',
+    },
+    {
+      icon: <SearchLogo />,
+      text: 'search',
+    },
+    {
+      icon: <NotificationsLogo />,
+      text: 'Notification',
+    },
+    {
+      icon: <CreatePostLogo />,
+      text: 'Create',
+    },
+    {
+      icon: <Avatar size={'sm'} name='Lavell Francis' src='/profile.png' />,
+      text: 'Profile',
+      link: '/lav3ll',
+    },
+  ];
   return (
     <Box
       height={'100vh'}
@@ -14,7 +46,6 @@ const Sidebar = () => {
       left={0}
       px={{ base: 2, md: 4 }}
     >
-      Sidebar
       <Flex direction={'column'} gap={10} w={'full'} height={'full'}>
         <Link
           to={'/'}
@@ -25,11 +56,21 @@ const Sidebar = () => {
         >
           <InstagramLogo />
         </Link>
+        <Link
+          to={'/'}
+          as={RouterLink}
+          pl={2}
+          display={{ base: 'block', md: 'none' }}
+          cursor={'pointer'}
+          borderRadius={6}
+          _hover={{ bg: 'whiteAlpha.200' }}
+          w={10}
+        >
+          <InstagramMobileLogo />
+        </Link>
       </Flex>
     </Box>
   );
-
-  sdd;
 };
 
 export default Sidebar;
