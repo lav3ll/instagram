55023cbb16adda172b0e6c5b7b0a55e9f74f1f45
lav3ll <lavell.francis@hotmail.com>
2024-06-19 23:04:24 +0100

    final visual changes made to postheader component

--- src/Components/FeedPosts/PostHeader.jsx
+++ src/Components/FeedPosts/PostHeader.jsx
@@ -1,13 +1,34 @@
-import { Flex, Box, Avatar } from '@chakra-ui/react';
+import { Flex, Box, Avatar, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const PostHeader = () => {
   return (
-    <Flex justifyContent={'space-between'} alignItems={'center'} w={'full'}>
+    <Flex
+      justifyContent={'space-between'}
+      alignItems={'center'}
+      w={'full'}
+      my={2}
+    >
       <Flex alignItems={'center'} gap={2}>
         <Avatar src='/img1.png' alt={'user profile pic'} size={'sm'} />
+        <Flex fontSize={'12px'} fontWeight={'bold'} gap={2}>
+          lavell
+        </Flex>
+        <Box color={'gray.500'}>•1w</Box>
       </Flex>
-      <Box> </Box>
+      <Box cursor={'pointer'}>
+        <Text
+          fontSize={12}
+          color={'blue.500'}
+          fontWeight={'bold'}
+          _hover={{
+            color: 'white',
+          }}
+          transition={'0.2s ease-in-out'}
+        >
+          Unfollow
+        </Text>
+      </Box>
     </Flex>
   );
 };
