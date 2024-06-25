30b8b3de94abddb89c22884066b97a15acaddb97
lav3ll <lavell.francis@hotmail.com>
2024-06-25 19:08:19 +0100

    fixed v stack to render in the correct position on the page

--- src/Components/FeedPosts/Feedposts.jsx
+++ src/Components/FeedPosts/Feedposts.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Container } from '@chakra-ui/react';
+import {
+  Container,
+  SkeletonCircle,
+  VStack,
+  Flex,
+  Skeleton,
+  Box,
+} from '@chakra-ui/react';
 import FeedPost from './FeedPost';
+import { useState, useEffect } from 'react';
 
 const Feedposts = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <Container
       maxW={'container.sm'}
@@ -10,10 +25,29 @@ const Feedposts = () => {
       px={2}
       data-testid='data-feedposts'
     >
-      <FeedPost username='lav3ll' avatar='/img1.png' img='/img1.png' />
-      <FeedPost username='norbert' avatar='/img2.png' img='/img2.png' />
-      <FeedPost username='lewis' avatar='/img3.png' img='/img3.png' />
-      <FeedPost username='cordell' avatar='/img4.png' img='/img4.png' />
+      {true &&
+        [0, 1, 2, 3].map((_, idx) => (
+          <VStack key={idx} gap={4} alignItems={'flex-start'} mb={10}>
+            <Flex gap={2}>
+              <SkeletonCircle size={10} />
+              <VStack gap={2} alignItems={'flex-start'}>
+                <SkeletonCircle w={'200px'} h={'10px'} />
+                <SkeletonCircle w={'200px'} h={'10px'} />
+              </VStack>
+            </Flex>
+            <Skeleton w={'full'}>
+              <Box h={'500px'}>contents wrapped</Box>
+            </Skeleton>
+          </VStack>
+        ))}
+      {!false && (
+        <>
+          <FeedPost username='lav3ll' avatar='/img1.png' img='/img1.png' />
+          <FeedPost username='norbert' avatar='/img2.png' img='/img2.png' />
+          <FeedPost username='lewis' avatar='/img3.png' img='/img3.png' />
+          <FeedPost username='cordell' avatar='/img4.png' img='/img4.png' />
+        </>
+      )}
     </Container>
   );
 };
