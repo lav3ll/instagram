f719a567873fb5f1d92ac9898f0f481075386557
lav3ll <lavell.francis@hotmail.com>
2024-06-26 23:22:37 +0100

    suggested logout button now links to auth page

--- src/Components/SuggestedUsers/SuggestedHeader.jsx
+++ src/Components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Flex, Avatar, Text } from '@chakra-ui/react';
+import { Flex, Avatar, Text, Link } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 
 const SuggestedHeader = () => {
   return (
     <Flex justifyContent={'space-between'} alignItems={'center'} w={'full'}>
       <Flex alignItems={'center'} gap={2}>
-        <Avatar name='lavell testing name' src='/img1.png' size={'lg'} />
+        <Avatar name='lavell testing name' src='/img1.png' size={'md'} />
         <Text fontSize={12} fontWeight={'bold'}>
           lavell
         </Text>
       </Flex>
+      <Link
+        as={RouterLink}
+        to={'/auth'}
+        fontSize={14}
+        fontWeight={'medium'}
+        color={'blue.400'}
+        style={{ textDecoration: 'none' }}
+        cursor={'pointer'}
+      >
+        Log Out
+      </Link>
     </Flex>
   );
 };
