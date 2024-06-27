35b28bcbd5f1dacf286c6c68c914faa75dbeda3b
lav3ll <lavell.francis@hotmail.com>
2024-06-27 21:50:38 +0100

    populated suggestedusers with dummy data

--- src/Components/SuggestedUsers/SuggestedUser.jsx
+++ src/Components/SuggestedUsers/SuggestedUser.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Avatar, Box, Button, Flex, VStack } from '@chakra-ui/react';
 
-const SuggestedUser = () => {
-  return <div>SuggestedUser</div>;
+const SuggestedUser = ({ followers, avatar, name }) => {
+  return (
+    <Flex justifyContent={'center'} alignItems={'center'} w={'full'}>
+      <Flex alignItems={'center'} gap={2}>
+        <Avatar src={avatar} name={name} size={'md'}></Avatar>
+        <VStack spacing={2}>
+          <Box fontSize={12} fontWeight={'bold'}>
+            {name}
+          </Box>
+          <Box fontSize={11} fontWeight={'gray.500'}>
+            {followers} followers
+          </Box>
+        </VStack>
+      </Flex>
+      <Button></Button>
+    </Flex>
+  );
 };
 
 export default SuggestedUser;
--- src/Components/SuggestedUsers/SuggestedUsers.jsx
+++ src/Components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,5 +1,6 @@
-import { VStack, Flex, Text } from '@chakra-ui/react';
+import { VStack, Flex, Text, Box, Link } from '@chakra-ui/react';
 import SuggestedHeader from './SuggestedHeader';
+import SuggestedUser from './SuggestedUser';
 import React from 'react';
 
 const SuggestedUsers = () => {
@@ -19,6 +20,33 @@ const SuggestedUsers = () => {
           See All
         </Text>
       </Flex>
+      <SuggestedUser
+        name='Norbert Osei'
+        followers={21}
+        avatar={'https://bit.ly/dan-abramov'}
+      />
+      <SuggestedUser
+        name='Mark Zuckerburg'
+        followers={21}
+        avatar={'https://bit.ly/ryan-florence'}
+      />
+      <SuggestedUser
+        name='Rey Mysterio'
+        followers={21}
+        avatar={'https://bit.ly/code-beast'}
+      />
+
+      <Box fontSize={12} color={'gray.500'} mt={5} alignSelf={'start'}>
+        @ 2024 Built By{' '}
+        <Link
+          href='https://github.com/lav3ll'
+          target={'blank'}
+          color={'blue.500'}
+          fontSize={14}
+        >
+          Lav3ll
+        </Link>
+      </Box>
     </VStack>
   );
 };
