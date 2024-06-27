dbe5b61d0a46f32338e34dd7a6c6d03ce36c1d46
lav3ll <lavell.francis@hotmail.com>
2024-06-27 21:12:11 +0100

    created base components to hold the suggested users

--- src/Components/SuggestedUsers/SuggestedUsers.jsx
+++ src/Components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,4 +1,4 @@
-import { VStack } from '@chakra-ui/react';
+import { VStack, Flex, Text } from '@chakra-ui/react';
 import SuggestedHeader from './SuggestedHeader';
 import React from 'react';
 
@@ -6,6 +6,19 @@ const SuggestedUsers = () => {
   return (
     <VStack py={8} px={6} gap={4}>
       <SuggestedHeader />
+      <Flex alignItems={'center'} justifyContent={'space-between'} w={'full'}>
+        <Text fontSize={12} fontWeight={'bold'} color={'gray.500'}>
+          Suggested for you
+        </Text>
+        <Text
+          fontSize={12}
+          fontWeight={'bold'}
+          _hover={{ color: 'gray.400' }}
+          cursor={'pointer'}
+        >
+          See All
+        </Text>
+      </Flex>
     </VStack>
   );
 };
